6c76f34c7f404b7b526276b36566a966247a7612
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-07 01:54:58 +0200

    Modification des fichiers de scripts

--- Create_Directory  .sh
+++ Create_Directory  .sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Ce script crée un répertoire nommé test_directory s'il n'existe pas déjà
-
-# Vérifie si le répertoire existe
-if [ -d "test_directory" ]; then
-    echo "Le répertoire 'test_directory' existe déjà."
-else
-    mkdir test_directory
-    echo "Répertoire 'test_directory' créé avec succès."
-fi
+# Ce script crée un répertoire nommé test_directory 
+mkdir -p test_directory
--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 # Ce script vérifie si un fichier existe ou non
+read filename
 
-# Demande à l'utilisateur le nom du fichier
-read -p "Entrez le nom du fichier : " filename
-
-# Vérifie si le fichier existe
 if [ -f "$filename" ]; then
   echo "Le fichier '$filename' existe"
 else
   echo "Le fichier '$filename' n'existe pas"
-fi
